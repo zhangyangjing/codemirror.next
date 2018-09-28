c605247991c442745ce6086bf18ace7c72fc981a
Adrian Heine <mail@adrianheine.de>
2018-09-28 09:38:26 +0200

    [legacy-modes] Separate caches

--- legacy-modes/src/index.ts
+++ legacy-modes/src/index.ts
@@ -1,9 +1,11 @@
+import {Text} from "../../doc/src/text"
 import {EditorView} from "../../view/src"
 import {Range} from "../../rangeset/src/rangeset"
-import {EditorState, Plugin, StateField, Transaction} from "../../state/src"
+import {EditorState, MetaSlot, Plugin, StateField, Transaction} from "../../state/src"
 import {Decoration} from "../../view/src/decoration"
 
 import {StringStreamCursor} from "./stringstreamcursor"
+import {StringStream} from "./stringstream"
 import {copyState, readToken, Mode} from "./util"
 
 class CachedState<S> {
@@ -21,64 +23,9 @@ function cutDecoratedRange(range: DecoratedRange | null, at: number) {
 }
 
 class StateCache<S> {
-  private timeout?: number | NodeJS.Timer
+  constructor(private states: CachedState<S>[], private frontier: number) {}
 
-  constructor(private states: CachedState<S>[], private frontier: number, private lastDecorations: null | DecoratedRange) {}
-
-  advanceFrontier(editorState: EditorState, to: number, mode: Mode<S>, sleepTime: number, maxWorkTime: number): Promise<void> {
-    if (this.frontier >= to) return Promise.reject()
-    clearTimeout(this.timeout as any)
-    return new Promise(resolve => {
-      const f = () => {
-        const endTime = +new Date + maxWorkTime
-        do {
-          const target = Math.min(to, this.frontier + MAX_SCAN_DIST / 2)
-          this.getState(editorState, target, mode)
-          if (this.frontier >= to) return resolve()
-        } while (+new Date < endTime)
-        this.timeout = setTimeout(f, sleepTime)
-      }
-      this.timeout = setTimeout(f, sleepTime)
-    })
-  }
-
-  private calculateDecorations(editorState: EditorState, from: number, to: number, mode: Mode<S>): Range<Decoration>[] {
-    let state = this.getState(editorState, from, mode)
-    let cursor = new StringStreamCursor(editorState.doc, from, editorState.tabSize)
-    let states: CachedState<S>[] = [], decorations: Range<Decoration>[] = [], stream = cursor.next()
-    for (let i = 0; cursor.offset + stream.start < to;) {
-      if (stream.eol()) {
-        stream = cursor.next()
-        if (++i % 5 == 0) states.push(new CachedState(copyState(mode, state), cursor.offset))
-      } else {
-        let style = readToken(mode, stream, state)
-        if (style)
-          decorations.push(Decoration.range(cursor.offset + stream.start, cursor.offset + stream.pos, {class: 'cm-' + style.replace(/ /g, ' cm-')}))
-        stream.start = stream.pos
-      }
-    }
-    this.storeStates(from, to, states)
-    return decorations
-  }
-
-  getDecorations(editorState: EditorState, from: number, to: number, mode: Mode<S>): Range<Decoration>[] {
-    let upto = from, decorations: Range<Decoration>[] = []
-    if (this.lastDecorations) {
-      if (from < this.lastDecorations.from) {
-        upto = Math.min(to, this.lastDecorations.from)
-        decorations = this.calculateDecorations(editorState, from, upto, mode)
-      }
-      if (upto < to && this.lastDecorations.to > upto) {
-        upto = this.lastDecorations.to
-        decorations = decorations.concat(this.lastDecorations.decorations)
-      }
-    }
-    if (upto < to) {
-      decorations = decorations.concat(this.calculateDecorations(editorState, upto, to, mode))
-    }
-    this.lastDecorations = {from, to, decorations}
-    return decorations
-  }
+  getFrontier(): number { return this.frontier }
 
   storeStates(from: number, to: number, states: ReadonlyArray<CachedState<S>>) {
     let start = this.findIndex(from), end = this.findIndex(to)
@@ -88,14 +35,14 @@ class StateCache<S> {
 
   // Return the first index for which all cached states after it have
   // a position >= pos
-  findIndex(pos: number): number {
+  private findIndex(pos: number): number {
     // FIXME could be binary search
     let i = 0
     while (i < this.states.length && this.states[i].pos < pos) i++
     return i
   }
 
-  stateBefore(pos: number, mode: Mode<S>): {state: S, pos: number} {
+  private stateBefore(pos: number, mode: Mode<S>): {state: S, pos: number} {
     if (pos > this.frontier && pos - this.frontier < MAX_SCAN_DIST) pos = this.frontier
     let index = this.findIndex(pos)
     if (index < this.states.length && this.states[index].pos == pos) index++
@@ -105,11 +52,6 @@ class StateCache<S> {
   getState(editorState: EditorState, pos: number, mode: Mode<S>): S {
     let {pos: statePos, state} = this.stateBefore(pos, mode)
     if (statePos < pos - MAX_SCAN_DIST) { statePos = pos; state = mode.startState() }
-    else if (this.lastDecorations && (statePos < this.lastDecorations.from && this.lastDecorations.from <= pos))
-      // If we are calculating a correct state for a position that is after the
-      // beginning of the cached decorations (which suggests that the cached
-      // decorations were rendered based on an approximate state), clear that cache
-      this.lastDecorations = null
     if (statePos < pos) {
       let cursor = new StringStreamCursor(editorState.doc, statePos, editorState.tabSize)
       let stream = cursor.next()
@@ -138,7 +80,117 @@ class StateCache<S> {
       let mapped = transaction.changes.mapPos(cached.pos, -1, true)
       if (mapped > 0) states.push(mapped == cached.pos ? cached : new CachedState(cached.state, mapped))
     }
-    return new StateCache(states, Math.min(start, this.frontier), cutDecoratedRange(this.lastDecorations, start))
+    return new StateCache(states, Math.min(start, this.frontier))
+  }
+}
+
+class TokenTypeCache<S> {
+  constructor(private states: StateCache<S>, private doc: Text, private tabSize: number, private mode: Mode<S>) {}
+
+  private calculateLine(lineNo: number, state?: S) {
+    const line = this.doc.line(lineNo)
+    state = state || this.states.getState({doc: this.doc, tabSize: this.tabSize}, line.start, this.mode)
+    const tokens = []
+    const stream = new StringStream(line.slice(), this.tabSize, null)
+    for (; !stream.eol(); stream.start = stream.pos) {
+      const type = readToken(this.mode, stream, state)
+      if (type) tokens.push({from: stream.start, to: stream.pos, type})
+    }
+    return {tokens, state}
+  }
+
+  typeAt(pos: number): string | null {
+    const line = this.doc.lineAt(pos)
+    const lineTokens = this.linesTokens(line.number, line.number)
+    for (const {from, to, type} of lineTokens)
+      if (from <= pos) return pos < to ? type : null
+    return null
+  }
+
+  linesTokens(fromLine: number, toLine: number): ReadonlyArray<{from: number, to: number, type: string}> {
+    const states = []
+    let result: {from: number, to: number, type: string}[] = []
+    let state
+    for (let lineNo = fromLine; lineNo <= toLine; ++lineNo) {
+      const offset = this.doc.line(lineNo).start
+      if (state && (lineNo - fromLine) % 5 == 0) states.push(new CachedState(copyState(this.mode, state), offset))
+      let tokens
+      ;({tokens, state} = this.calculateLine(lineNo, state))
+      result = result.concat(tokens.map(({from, to, type}) => ({from: from + offset, to: to + offset, type})))
+    }
+    const endPos = this.doc.line(toLine).end + 1
+    if (state && toLine % 5 == 0) states.push(new CachedState(copyState(this.mode, state), endPos))
+    this.states.storeStates(this.doc.line(fromLine).start, endPos, states)
+    return result
+  }
+
+  apply(stateCache: StateCache<S>, transaction: Transaction): TokenTypeCache<S> {
+    const newTabSize = transaction.getMeta(MetaSlot.changeTabSize)
+    return new TokenTypeCache(stateCache, transaction.doc, newTabSize !== undefined ? newTabSize : transaction.startState.tabSize, this.mode)
+  }
+}
+
+class StyleCache<S> {
+  private timeout?: number | NodeJS.Timer
+
+  constructor(private states: StateCache<S>, private tokens: TokenTypeCache<S>, private lastDecorations: null | DecoratedRange) {}
+
+  advanceFrontier(editorState: EditorState, to: number, mode: Mode<S>, sleepTime: number, maxWorkTime: number): Promise<void> {
+    if (this.states.getFrontier() >= to) return Promise.reject()
+    clearTimeout(this.timeout as any)
+    return new Promise(resolve => {
+      const f = () => {
+        const endTime = +new Date + maxWorkTime
+        do {
+          const frontierBefore = this.states.getFrontier()
+          const target = Math.min(to, frontierBefore + MAX_SCAN_DIST / 2)
+          if (this.lastDecorations && frontierBefore < this.lastDecorations.from && this.lastDecorations.from <= target){
+            this.lastDecorations = null
+}
+          this.states.getState(editorState, target, mode)
+          if (this.states.getFrontier() >= to) return resolve()
+        } while (+new Date < endTime)
+        this.timeout = setTimeout(f, sleepTime)
+      }
+      this.timeout = setTimeout(f, sleepTime)
+    })
+  }
+
+  private calculateDecorations(doc: Text, from: number, to: number, mode: Mode<S>): Range<Decoration>[] {
+    const fromLine = doc.lineAt(from)
+    const toLine = doc.lineAt(to)
+    const tokens = this.tokens.linesTokens(fromLine.number, to == toLine.start ? toLine.number - 1 : toLine.number)
+    let decorations = []
+    for (const token of tokens) {
+      if (token.from < to && token.to >= from) decorations.push(Decoration.range(token.from, token.to, {class: 'cm-' + token.type.replace(/ /g, ' cm-')}))
+    }
+    return decorations
+  }
+
+  getDecorations(doc: Text, from: number, to: number, mode: Mode<S>): Range<Decoration>[] {
+    let upto = from, decorations: Range<Decoration>[] = []
+    if (this.lastDecorations) {
+      if (from < this.lastDecorations.from) {
+        upto = Math.min(to, this.lastDecorations.from)
+        decorations = this.calculateDecorations(doc, from, upto, mode)
+      }
+      if (upto < to && this.lastDecorations.to > upto) {
+        upto = this.lastDecorations.to
+        decorations = decorations.concat(this.lastDecorations.decorations)
+      }
+    }
+    if (upto < to) {
+      decorations = decorations.concat(this.calculateDecorations(doc, upto, to, mode))
+    }
+    this.lastDecorations = {from, to, decorations}
+    return decorations
+  }
+
+  apply(transaction: Transaction): StyleCache<S> {
+    if (transaction.changes.length == 0) return this
+    let {start} = transaction.doc.lineAt(transaction.changes.changes.reduce((m, ch) => Math.min(m, ch.from), 1e9))
+    const states = this.states.apply(transaction)
+    return new StyleCache(states, this.tokens.apply(states, transaction), cutDecoratedRange(this.lastDecorations, start))
   }
 }
 
@@ -149,8 +201,10 @@ type Config = {
 
 export function legacyMode<S>(mode: Mode<S>, config: Config = {}) {
   const {sleepTime = 100, maxWorkTime = 100} = config
-  const field = new StateField<StateCache<S>>({
-    init(state: EditorState) { return new StateCache([], 0, null) },
+  const field = new StateField<StyleCache<S>>({
+    init(state: EditorState) {
+      const stateCache = new StateCache<S>([], 0)
+      return new StyleCache(stateCache, new TokenTypeCache<S>(stateCache, state.doc, state.tabSize, mode), null) },
     apply(tr, cache) { return cache.apply(tr) },
     debugName: "mode"
   })
@@ -164,7 +218,7 @@ export function legacyMode<S>(mode: Mode<S>, config: Config = {}) {
         if (force || vp.from < from || vp.to > to) {
           ;({from, to} = vp)
           const stateCache = v.state.getField(field)!
-          decorations = Decoration.set(stateCache.getDecorations(v.state, from, to, mode))
+          decorations = Decoration.set(stateCache.getDecorations(v.state.doc, from, to, mode))
           stateCache.advanceFrontier(v.state, from, mode, sleepTime, maxWorkTime).then(() => {
             update(v, true)
             v.decorationUpdate()
@@ -183,7 +237,7 @@ export function legacyMode<S>(mode: Mode<S>, config: Config = {}) {
   // not sure yet what it'd look like
   ;(plugin as any).indentation = function(state: EditorState, pos: number): number {
     if (!mode.indent) return -1
-    let modeState = state.getField(field)!.getState(state, pos, mode)
+    let modeState = state.getField(field)!.states.getState(state, pos, mode)
     let line = state.doc.lineAt(pos)
     return mode.indent(modeState, line.slice(0, Math.min(line.length, 100)).match(/^\s*(.*)/)![1])
   }
